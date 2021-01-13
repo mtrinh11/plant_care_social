c0ff889e42b74b33bbe6ea201b2f84bac23594a4
mtrinhwincomp <mtrinh11@gmail.com>
2021-01-13 15:03:41 -0800

    change static

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, 'client', 'build', 'index.html')))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.use('/api', AppRouter)
 
